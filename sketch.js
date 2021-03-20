33df824e68394133988504fb16628ae50f13a11d
TrexSamar <75522798+TrexSamar@users.noreply.github.com>
2021-03-19 20:20:45 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var wall;
+var bullet, speed, weight;
+
+var speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed=random(55,90)
+  weight=random(400,1500)
+
+  bullet=createSprite(50, 200, 50, 5 );
+  bullet.velocityX=speed;
+  bullet.shapeColor=color(255)
+
+  thickness=random(22, 83)
+
+  wall=createSprite(1200, 200, thickness, height/2)
+  wall.shapeColor=(80,80,80);
+}
+function draw() {
+  background(0);  
+   if(wall.x-bullet.x < (bullet.width+wall.width/2))
+ {
+  bullet.velocityX=0;
+  var deformation=0.5 * weight * speed * speed/ thickness * thickness * thickness;
+  if(deformation>10){
+    wall.shapeColor=color(255,0,0);
+  }
+  if(deformation<10){
+    wall.shapeColor=color(230,230,0);
+  }
+   
+   }
+   drawSprites();
+}
